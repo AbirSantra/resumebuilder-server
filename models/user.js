04514ae84d4745484f16e3b6799c7c90d78405c7
Abir Santra <santraabir8@gmail.com>
2022-10-25 21:46:02 +0530

    added user and resume models

---
+++ models/user.js
@@ -0,0 +1,38 @@
+import mongoose, { Mongoose } from "mongoose";
+
+// User Schema definition
+const userSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		firstname: {
+			type: String,
+			required: true,
+		},
+		middlename: {
+			type: String,
+		},
+		lastname: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+// User Model definition
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
